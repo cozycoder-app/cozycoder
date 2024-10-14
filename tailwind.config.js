773087502577ea6b7f7d2f966e00423695e148ce
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-10-14 07:18:02 +0200

    Add Rose Pine theme
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{gleam,mjs}"],
   theme: {
-    extend: {},
-  },
+     extend: {
+      colors: {
+        rosepine: {
+          base: '#191724',
+          surface: '#1f1d2e',
+          overlay: '#26233a',
+          muted: '#6e6a86',
+          subtle: '#908caa',
+          text: '#e0def4',
+          love: '#eb6f92',
+          gold: '#f6c177',
+          rose: '#ebbcba',
+          pine: '#31748f',
+          foam: '#9ccfd8',
+          iris: '#c4a7e7',
+          highlightLow: '#21202e',
+          highlightMed: '#403d52',
+          highlightHigh: '#524f67',
+        },
+      },
+    }, },
   plugins: [],
 };
